9e71ee53a2c766e4761ea7d8df29391f48021613
CardosoB8 <166424120+CardosoB8@users.noreply.github.com>
2026-03-03 13:07:44 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -643,6 +643,7 @@ app.get('/step', (req, res) => {
 <head>
     <title>Etapa ${payload.step} de 6</title>
     <meta charset="UTF-8">
+    <script src="https://quge5.com/88/tag.min.js" data-zone="216045" async data-cfasync="false"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes">
     <style>
         * {
